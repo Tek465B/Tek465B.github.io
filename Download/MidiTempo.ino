a83767b1bdf549d4a4405f4591cfc4c1bb8587ab
Mathieu <gamegunsz28@gmail.com>
2021-04-27 18:34:42 -0400

    Update MidiTempo.ino

--- Download/MidiTempo.ino
+++ Download/MidiTempo.ino
@@ -1,6 +1,6 @@
 /*
  * Midi/USB BPM display
- * Use 7 segment LED display common Anode with 2N3906 PNP transistors and 74HV595 shift register.
+ * Use 7 segment LED display common Anode with 2N3906 PNP transistors and 74HC595 shift register.
  * H11L1 opto-cpupler and 74HC14 inverter.
  * 
  * 2021-04-27
